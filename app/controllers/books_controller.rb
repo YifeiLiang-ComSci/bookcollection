1978d49377b9fc77e45690eb6b83d928874ecd7d
Yifei Liang <yifei.liang@tamu.edu>
2021-01-28 21:54:16 -0600

    created add new book functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,25 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.order("title")
+    @books = Book.order("id")
   end
 
   def show
+    @book = Book.new
   end
 
   def new
+    @book = Book.new
+
+    
   end
   
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to(books_path)
+    else
+      render('new')
+    end
 
   end
 
@@ -25,5 +35,8 @@ class BooksController < ApplicationController
 
   def destroy
   end
-
+private 
+  def book_params
+    params.require(:book).permit(:title,:author,:genre,:price,:publishe_date)
+  end
 end
